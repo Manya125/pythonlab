3f050fe832c6564328fecc4e1d44909d2ea4702e
Manya M <93362285+Manya125@users.noreply.github.com>
2022-02-06 02:30:43 -0800

    Add files via upload

---
+++ prgm2_co5.py
@@ -0,0 +1,18 @@
+fileobj=open("file2.txt","r+")
+filelist=[]
+
+condition=True
+
+while condition:
+    x = fileobj.readline()
+    filelist.append(x)
+    if not x:
+        print("file ended")
+        condition=False
+
+fileobjnew=open('file1.txt','a')
+for i in range(0,len(filelist),2):
+    fileobjnew.write(filelist[i])
+    
+fileobjnew.close()
+fileobj.close()
