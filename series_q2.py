d5cc1c0fe17d68a983de4fed052caa4afec24d1f
Manya M <93362285+Manya125@users.noreply.github.com>
2022-02-06 21:00:30 -0800

    Add files via upload

---
+++ series_q2.py
@@ -0,0 +1,17 @@
+file_obj = open('prgm2.txt','r+')
+
+condition = True
+
+
+print("The line that have more than 5 letters and starting with capital letter is:")
+
+while condition:
+    line = file_obj.readline()
+    words = line.split(" ")
+    if(len(words)>5):
+        x = words[0]
+        if(x[0].isupper()):
+            print(line)
+    if not line:
+        condition = False
+        print("end of the file")
